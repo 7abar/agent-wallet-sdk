f1e91d9dbe15d7a25d547d0a894d857d467fb76c
Jabar <jabarxheroes@gmail.com>
2026-03-13 22:19:10 +0700

    add src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+export { AgentWallet } from "./wallet.js";
+export type {
+  AgentWalletConfig,
+  SendEthParams,
+  SendTokenParams,
+  ContractCallParams,
+  WalletBalance,
+  TokenBalance,
+  TxReceipt,
+  SignedMessage,
+  WatchEventParams,
+  Address,
+  Hash,
+  Hex,
+} from "./types.js";
